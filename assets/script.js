bc6140db9e7969cb44a37da2714bd6f31224946a
Timanana <72578464+Timanana@users.noreply.github.com>
2023-02-05 09:49:25 -0800

    no more

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-  // add stuff to the navbar
-  fetch("https://script.google.com/macros/s/AKfycbwoygN-ilaOfgUP-IxC06fvY4bp9R_JovxMHem--ROxFbvuG1zG4xCEUREmxz1XrpecgA/exec").then(e => e.text()).then(e => {
-    navbarhtml.outerHTML = e;
-  });
-};
